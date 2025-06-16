40bb96e41327a525668b7b232515bef727a11b7c
martingerardoc <martingerardoc56@gmail.com>
2025-06-16 00:59:08 -0300

    pequeño mensaje

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+// EJERSICIO 1
+ // Crear un array de frutas y agregarle al final "UVA"
+
+ const frutas =["manzana", "platano", "naranja"];
+
+ console.log("el array original es de");
+
+ frutas.push("uva");
+
+ console.log("El array modificado es:" + frutas);
+
+ // para eliminar el ultimo elemento utilizamos pop
+
+ frutas.pop();
+
+ console.log("el array despues de eliminar el ultimo elemento es :" + frutas);
+
+ 
